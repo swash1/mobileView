ab417ebf14140cbd1bcd5f47c42bd3fd26b9850a
Tim Khayretdinov <timur3063@yandex.ru>
2024-06-05 17:48:38 +0300

    bottom bar

--- script.js
+++ script.js
@@ -6,120 +6,46 @@ const input = document.querySelector(".input");
 const wrapper = document.querySelector(".wrapper");
 const info = document.querySelector(".info");
 
-const updateStyles = (newHeight) => {
-  bubbles.style.top = `${newHeight}px`;
-};
-
-let pendingUpdate = false;
-
-function viewportHandler() {
-  if (pendingUpdate) return;
-  pendingUpdate = true;
-
-  requestAnimationFrame(() => {
-    pendingUpdate = false;
-
-    info.innerHTML = window.visualViewport.offsetTop;
-
-    // Stick to top
-    wrapper.style.transform = `translateY(${Math.max(
-      0,
-      window.visualViewport.offsetTop
-    )}px)`;
-
-    // Stick to bottom
-    if (window.visualViewport.offsetTop >= 0) {
-      bubbles.style.transform = `translateY(-${Math.max(
-        40,
-        window.innerHeight -
-          window.visualViewport.height -
-          window.visualViewport.offsetTop
-      )}px)`;
-    }
-  });
-}
-
-if (window.visualViewport) {
-  window.visualViewport.addEventListener("resize", () =>
-    viewportHandler(window.visualViewport.height)
-  );
-  window.visualViewport.addEventListener("scroll", () =>
-    viewportHandler(window.visualViewport.height)
-  );
-  updateStyles(window.visualViewport.height);
-} else {
-  window.addEventListener("resize", () => viewportHandler(window.innerHeight));
-  window.addEventListener("scroll", () => viewportHandler(window.innerHeight));
-}
-
-viewportHandler();
-
 input.addEventListener("focus", () => {
-  tabs.setAttribute("hidden", "true");
+  // tabs.setAttribute("hidden", "true");
   actions.setAttribute("hidden", "true");
   bubbles.removeAttribute("hidden");
 });
 
 input.addEventListener("blur", () => {
-  tabs.removeAttribute("hidden");
+  // tabs.removeAttribute("hidden");
   actions.removeAttribute("hidden");
   bubbles.setAttribute("hidden", "true");
 });
 
-// function debounce(callee, timeoutMs) {
-//   return function perform(...args) {
-//     let previousCall = this.lastCall;
+const bottomBar = document.querySelector(".bottombar");
+const viewport = window.visualViewport;
+let pendingUpdate = false;
 
-//     this.lastCall = Date.now();
+function viewportHandler(event) {
+  if (pendingUpdate) return;
+  pendingUpdate = true;
 
-//     if (previousCall && this.lastCall - previousCall <= timeoutMs) {
-//       clearTimeout(this.lastCallTimer);
-//     }
+  requestAnimationFrame(() => {
+    pendingUpdate = false;
+    const layoutViewport = document.getElementById("layoutViewport");
 
-//     this.lastCallTimer = setTimeout(() => callee(...args), timeoutMs);
-//   };
-// }
+    // Since the bar is position: fixed we need to offset it by the
+    // visual viewport's offset from the layout viewport origin.
+    const viewport = event.target;
+    const offsetLeft = viewport.offsetLeft;
+    const offsetTop =
+      viewport.height -
+      layoutViewport.getBoundingClientRect().height +
+      viewport.offsetTop;
 
-// let fixPaginatorPosition = 0; // the fix
-// let lastScrollY = window.scrollY; // the last scroll position
-// let wrapper = document.querySelector(".wrapper"); // the toolbar wrap
+    // You could also do this by setting style.left and style.top if you
+    // use width: 100% instead.
+    bottomBar.style.transform = `translate(${offsetLeft}px, ${offsetTop}px) scale(${
+      1 / viewport.scale
+    })`;
+  });
+}
 
-// // function to set the margin to show the toolbar if hidden
-// const setMargin = function () {
-//   // if toolbar wrap is hidden
-//   const newPosition = wrapper.getBoundingClientRect().top;
-//   if (newPosition < -1) {
-//     // add a margin to show the toolbar
-//     anotherWrapper.classList.add("down"); // add class so toolbar can be animated
-//     fixPaginatorPosition = Math.abs(newPosition); // this is new position we need to fix the toolbar in the display
-//     // if at the bottom of the page take a couple of pixels off due to gap
-//     if (window.innerHeight + window.scrollY >= document.body.offsetHeight) {
-//       fixPaginatorPosition -= 2;
-//     }
-//     // set the margin to the new fixed position
-//     anotherWrapper.style["margin-top"] = fixPaginatorPosition + "px";
-//   }
-// };
-
-// // use lodash debounce to stop flicker
-// const debounceMargin = debounce(setMargin, 150);
-
-// // function to run on scroll and blur
-// const showToolbar = function () {
-//   // remove animation and put toolbar back in default position
-//   if (fixPaginatorPosition > 0) {
-//     anotherWrapper.classList.remove("down");
-//     fixPaginatorPosition = 0;
-//     anotherWrapper.style["margin-top"] = 0 + "px";
-//   }
-
-//   // will check if toolbar needs to be fixed
-//   debounceMargin();
-// };
-
-// // add an event listener to scroll to check if
-// // toolbar position has moved off the page
-// window.addEventListener("scroll", showToolbar);
-// // add an event listener to blur as iOS keyboard may have closed
-// // and toolbar postition needs to be checked again
-// input.addEventListener("blur", showToolbar);
+window.visualViewport.addEventListener("scroll", viewportHandler);
+window.visualViewport.addEventListener("resize", viewportHandler);
