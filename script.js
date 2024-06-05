cceaf8e74c3fc0f187516cfd2c4685f31720fb08
Tim Khayretdinov <timur3063@yandex.ru>
2024-06-05 15:59:25 +0300

    попытка

--- script.js
+++ script.js
@@ -3,20 +3,56 @@ const paginator = document.querySelector(".paginator");
 const bubbles = document.querySelector(".bubbles");
 const actions = document.querySelector(".actions");
 const input = document.querySelector(".input");
-const anotherWrapper = document.querySelector(".another-wrapper");
+const wrapper = document.querySelector(".wrapper");
+const info = document.querySelector(".info");
 
 const updateStyles = (newHeight) => {
   bubbles.style.top = `${newHeight}px`;
 };
 
+let pendingUpdate = false;
+
+function viewportHandler() {
+  if (pendingUpdate) return;
+  pendingUpdate = true;
+
+  requestAnimationFrame(() => {
+    pendingUpdate = false;
+
+    info.innerHTML = window.visualViewport.offsetTop;
+
+    // Stick to top
+    wrapper.style.transform = `translateY(${Math.max(
+      0,
+      window.visualViewport.offsetTop
+    )}px)`;
+
+    // Stick to bottom
+    if (window.visualViewport.offsetTop >= 0) {
+      bubbles.style.transform = `translateY(-${Math.max(
+        40,
+        window.innerHeight -
+          window.visualViewport.height -
+          window.visualViewport.offsetTop
+      )}px)`;
+    }
+  });
+}
+
 if (window.visualViewport) {
   window.visualViewport.addEventListener("resize", () =>
-    updateStyles(window.visualViewport.height)
+    viewportHandler(window.visualViewport.height)
+  );
+  window.visualViewport.addEventListener("scroll", () =>
+    viewportHandler(window.visualViewport.height)
   );
   updateStyles(window.visualViewport.height);
 } else {
-  window.addEventListener("resize", () => updateStyles(window.innerHeight));
+  window.addEventListener("resize", () => viewportHandler(window.innerHeight));
+  window.addEventListener("scroll", () => viewportHandler(window.innerHeight));
 }
+
+viewportHandler();
 
 input.addEventListener("focus", () => {
   tabs.setAttribute("hidden", "true");
@@ -30,60 +66,60 @@ input.addEventListener("blur", () => {
   bubbles.setAttribute("hidden", "true");
 });
 
-function debounce(callee, timeoutMs) {
-  return function perform(...args) {
-    let previousCall = this.lastCall;
+// function debounce(callee, timeoutMs) {
+//   return function perform(...args) {
+//     let previousCall = this.lastCall;
 
-    this.lastCall = Date.now();
+//     this.lastCall = Date.now();
 
-    if (previousCall && this.lastCall - previousCall <= timeoutMs) {
-      clearTimeout(this.lastCallTimer);
-    }
+//     if (previousCall && this.lastCall - previousCall <= timeoutMs) {
+//       clearTimeout(this.lastCallTimer);
+//     }
 
-    this.lastCallTimer = setTimeout(() => callee(...args), timeoutMs);
-  };
-}
+//     this.lastCallTimer = setTimeout(() => callee(...args), timeoutMs);
+//   };
+// }
 
-let fixPaginatorPosition = 0; // the fix
-let lastScrollY = window.scrollY; // the last scroll position
-let wrapper = document.querySelector(".wrapper"); // the toolbar wrap
+// let fixPaginatorPosition = 0; // the fix
+// let lastScrollY = window.scrollY; // the last scroll position
+// let wrapper = document.querySelector(".wrapper"); // the toolbar wrap
 
-// function to set the margin to show the toolbar if hidden
-const setMargin = function () {
-  // if toolbar wrap is hidden
-  const newPosition = wrapper.getBoundingClientRect().top;
-  if (newPosition < -1) {
-    // add a margin to show the toolbar
-    anotherWrapper.classList.add("down"); // add class so toolbar can be animated
-    fixPaginatorPosition = Math.abs(newPosition); // this is new position we need to fix the toolbar in the display
-    // if at the bottom of the page take a couple of pixels off due to gap
-    if (window.innerHeight + window.scrollY >= document.body.offsetHeight) {
-      fixPaginatorPosition -= 2;
-    }
-    // set the margin to the new fixed position
-    anotherWrapper.style["margin-top"] = fixPaginatorPosition + "px";
-  }
-};
+// // function to set the margin to show the toolbar if hidden
+// const setMargin = function () {
+//   // if toolbar wrap is hidden
+//   const newPosition = wrapper.getBoundingClientRect().top;
+//   if (newPosition < -1) {
+//     // add a margin to show the toolbar
+//     anotherWrapper.classList.add("down"); // add class so toolbar can be animated
+//     fixPaginatorPosition = Math.abs(newPosition); // this is new position we need to fix the toolbar in the display
+//     // if at the bottom of the page take a couple of pixels off due to gap
+//     if (window.innerHeight + window.scrollY >= document.body.offsetHeight) {
+//       fixPaginatorPosition -= 2;
+//     }
+//     // set the margin to the new fixed position
+//     anotherWrapper.style["margin-top"] = fixPaginatorPosition + "px";
+//   }
+// };
 
-// use lodash debounce to stop flicker
-const debounceMargin = debounce(setMargin, 150);
+// // use lodash debounce to stop flicker
+// const debounceMargin = debounce(setMargin, 150);
 
-// function to run on scroll and blur
-const showToolbar = function () {
-  // remove animation and put toolbar back in default position
-  if (fixPaginatorPosition > 0) {
-    anotherWrapper.classList.remove("down");
-    fixPaginatorPosition = 0;
-    anotherWrapper.style["margin-top"] = 0 + "px";
-  }
+// // function to run on scroll and blur
+// const showToolbar = function () {
+//   // remove animation and put toolbar back in default position
+//   if (fixPaginatorPosition > 0) {
+//     anotherWrapper.classList.remove("down");
+//     fixPaginatorPosition = 0;
+//     anotherWrapper.style["margin-top"] = 0 + "px";
+//   }
 
-  // will check if toolbar needs to be fixed
-  debounceMargin();
-};
+//   // will check if toolbar needs to be fixed
+//   debounceMargin();
+// };
 
-// add an event listener to scroll to check if
-// toolbar position has moved off the page
-window.addEventListener("scroll", showToolbar);
-// add an event listener to blur as iOS keyboard may have closed
-// and toolbar postition needs to be checked again
-input.addEventListener("blur", showToolbar);
+// // add an event listener to scroll to check if
+// // toolbar position has moved off the page
+// window.addEventListener("scroll", showToolbar);
+// // add an event listener to blur as iOS keyboard may have closed
+// // and toolbar postition needs to be checked again
+// input.addEventListener("blur", showToolbar);
