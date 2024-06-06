256b81b1b8bffb8f96393a861cd317d7e989fa72
Tim Khayretdinov <timur3063@yandex.ru>
2024-06-06 15:35:07 +0300

    дебаг + ПЗ

--- script.js
+++ script.js
@@ -6,10 +6,10 @@ const input = document.querySelector(".input");
 const topBar = document.querySelector(".topBar");
 const bottomBar = document.querySelector(".bottomBar");
 
-function debounce(fn, delay) {
+debounce = (fn, delay) => {
   let timerId;
 
-  return function (...args) {
+  return (...args) => {
     // Если таймер уже существует, очистим его (сбросим время)
     if (timerId) {
       clearTimeout(timerId);
@@ -20,15 +20,17 @@ function debounce(fn, delay) {
       fn(...args);
     }, delay);
   };
-}
+};
 
+// Если нажимаем на сниппет, не снимать фокус с инпута и не скрывать клавиатуру
 const focusOnInput = (e) => {
   e.preventDefault();
   input.focus();
 };
-
 snippets.addEventListener("mousedown", focusOnInput);
 
+// При фокусе на клавиатуру прячем ненужные элементы и отображаем нужные
+// Можно будет прикрутить анимацию появления, чтобы было поприятнее
 input.addEventListener("focus", () => {
   tabs.setAttribute("hidden", "true");
   controls.setAttribute("hidden", "true");
@@ -41,49 +43,60 @@ input.addEventListener("blur", () => {
   snippets.setAttribute("hidden", "true");
 });
 
-const viewport = window.visualViewport;
+// Предотваращет лишние перерисовки, т.к. ивент вызывает на resize и scroll, а эти события могут происходить одновременно
 let pendingUpdate = false;
 
-function viewportHandler(event) {
+const viewportHandler = (event) => {
   if (pendingUpdate) return;
   pendingUpdate = true;
 
+  // requestAnimationFrame гарантирует что изменения произойдут перед следующим рендерингом
   requestAnimationFrame(() => {
     pendingUpdate = false;
     const layoutViewport = document.querySelector(".layoutViewport");
 
-    // Since the bar is position: fixed we need to offset it by the
-    // visual viewport's offset from the layout viewport origin.
+    // т.к. позиция верхнего и нижнего баров с position fixed считается
+    // для layout viewport, то нам надо сделать некоторые расчеты
+    // чтобы такие элементы спозиционировались для visual viewport
     const viewport = event.target;
+    // Если пользователь увеличил экран и у него есть какой-то отступ от краев
     const offsetLeft = viewport.offsetLeft;
-    const offsetTop =
+    // Считаем насколько надо поднять bottom bar
+    const offsetTopForBottomBar =
       viewport.height -
       layoutViewport.getBoundingClientRect().height +
       viewport.offsetTop;
 
-    // You could also do this by setting style.left and style.top if you
-    // use width: 100% instead.
-    bottomBar.style.transform = `translate(${offsetLeft}px, ${offsetTop}px) scale(${
+    // document.querySelector("#height").innerHTML = "vvh " + viewport.height;
+    // document.querySelector("#layoutViewport").innerHTML =
+    //   "lvh " + layoutViewport.getBoundingClientRect().height;
+    // document.querySelector("#offsetTop").innerHTML = "ot " + viewport.offsetTop;
+
+    // Поднимаем bottom bar
+    bottomBar.style.transform = `translate(${offsetLeft}px, ${offsetTopForBottomBar}px) scale(${
       1 / viewport.scale
     })`;
 
+    // Опускаем bottom bar, если позиция верха visual viewport != позиции верха layout viewport
     topBar.style.transform = `translate(${offsetLeft}px, ${
       viewport.offsetTop
     }px) scale(${1 / viewport.scale})`;
-
-    bottomBar.removeAttribute("hidden");
-    topBar.removeAttribute("hidden");
   });
-}
+
+  bottomBar.removeAttribute("hidden");
+  topBar.removeAttribute("hidden");
+};
 
 const showBars = () => {
   bottomBar.removeAttribute("hidden");
   topBar.removeAttribute("hidden");
 };
 
+// Дебаунс чтобы предотвратить "мерцания" во время скрытия и показа
 const debouncedViewportHandler = debounce(viewportHandler, 500);
 const debouncedShowBars = debounce(showBars, 500);
 
+// Прячем когда начали скроллить, тк например айфон переопределяет параметры visual viewport только после окончания скролла, и элементы во время начала скролла остаются на своем месте и улетают вместе со страницей.
 const handleViewport = (e) => {
   bottomBar.setAttribute("hidden", "true");
   topBar.setAttribute("hidden", "true");
